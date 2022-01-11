9167a84937dad56bc7b91cc7f3a79a2bbdc3a881
Victor Dolsan <victor.freitas@mazzafc.tech>
2022-01-10 21:50:40 -0300

    Adicionado arquivo inicial PHP

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<h1>Hello World!</h1>";
+    echo "Meu nome é Victor<br>";
+    echo "Sou programador WEB!";
+    printf("<p>Hoje é segunda!</p>");
+    echo "<p>Estou aprendendo PHP</p>"
+?>
